e41e7d5c42a27f85014c296f06b695ba0006b380
Nikita Vasilyaka <NikelVarner@gmail.com>
2023-04-24 21:50:41 +0300

    ів

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+const lightbox = document.querySelector('.lightbox');
+const lightboxImage = document.querySelector('.lightbox__image');
+
+galleryList.addEventListener('click', onGalleryItemClick);
+
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>
+        </li>
+      `;
+    })
+    .join('');
+}
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+
+  const galleryItemImage = event.target;
+  if (!galleryItemImage.classList.contains('gallery__image')) {
+    return;
+  }
+
+  lightbox.classList.add('is-open');
+  lightboxImage.src = galleryItemImage.dataset.source;
+  lightboxImage.alt = galleryItemImage.alt;
+}
+
+lightbox.addEventListener('click', onLightboxClick);
+
+function onLightboxClick(event) {
+  const lightboxOverlay = event.target;
+  if (!lightboxOverlay.classList.contains('lightbox__content')) {
+    return;
+  }
+
+  lightbox.classList.remove('is-open');
+  lightboxImage.src = '';
+  lightboxImage.alt = '';
+}
